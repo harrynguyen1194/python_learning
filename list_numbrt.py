3145258c835ee3281587396e60e70f0e650cf566
Harry Nguyen <hoang.ng1194@gmail.com>
2022-10-23 16:11:56 +1030

    Update list function

--- list_numbrt.py
+++ list_numbrt.py
@@ -1,9 +1,15 @@
-num = [1,2,3,4,5,6,7,8,9,10]
-for item in num:
-    print(item)
+# num = [1,2,3,4,5,6,7,8,9,10]
+# for item in num:
+#     print(item)
+#
+#
+# i = 0
+# while i < len(num):
+#     print(num[i])
+#     i =i+1
 
-
-i = 0
-while i < len(num):
-    print(num[i])
-    i =i+1
+lucky_number =[4,8,15,16,23,42]
+friend = ["Kevin","Karen","Jim","Oscar"]
+friend.append("Creed")
+friend.extend(lucky_number)
+print(friend)
