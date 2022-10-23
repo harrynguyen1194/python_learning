a1c005c77e5c70606d4e54aec58ebca32a40bafd
Harry Nguyen <hoang.ng1194@gmail.com>
2022-10-23 19:00:06 +1030

    Update function with parameter Update if statement with function and return Update return with funcion

---
+++ function_def.py
@@ -0,0 +1,15 @@
+# def say_hi():
+#     print("Hello world")
+# print("Top")
+# say_hi()
+# print("Bottom")
+
+
+
+def intro(name,age):
+    print("Hi " +name+ " How are you")
+    print("You are " + age)
+
+
+intro("Harry","27")
+intro("Bellar","90")
