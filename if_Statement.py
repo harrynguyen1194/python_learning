600b782995891f5714cde431a87154576203faf7
Harry Nguyen <hoang.ng1194@gmail.com>
2022-10-26 18:04:43 +1030

    Update if statement with operation

--- if_Statement.py
+++ if_Statement.py
@@ -13,12 +13,36 @@
 
 
 
-def max_num(num1,num2,num3):
-    if num1>=num2 and num1>=num3:
-        return num1
-    elif num2>=num1 and num2>=num3:
-        return num2
-    else:
-        return num3
+# def max_num(num1,num2,num3):
+#     if num1>=num2 and num1>=num3:
+#         return num1
+#     elif num2>=num1 and num2>=num3:
+#         return num2
+#     else:
+#         return num3
+#
+# print(max_num(3000,40,5000000 ))
+#
 
-print(max_num(3000,40,5000000))
+sun = True
+rain = False
+
+if sun:
+    print("Bring sunglasses")
+elif rain:
+    print("Bring umberlla")
+
+#The operation with 2 numbers
+num1 = float(input("Enter first num: "))
+num2 = float(input("Enter second num: "))
+op= input("Enter operation: ")
+if op == "+":
+    print(num1+num2)
+elif op =="-":
+    print(num1-num2)
+elif op =="*":
+    print(num1*num2)
+elif op=="/":
+    print(num1/num2)
+else:
+    print("error")
